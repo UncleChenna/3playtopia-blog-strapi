51eb49a8d5cac8a599599a8d7c5d5ed7c78b378c
UncleChenna <gentlebeast02@outlook.com>
2025-10-06 09:08:34 +0100

    mmmm

--- config/admin.ts
+++ config/admin.ts
@@ -2,7 +2,7 @@ export default ({ env }) => {
   console.log('ADMIN_JWT_SECRET:', env('ADMIN_JWT_SECRET') ? 'Found' : 'Missing');
   return {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env('ADMIN_JWT_SECRET') || 'DznXVuuSwARmxnbE34slmg==',
     },
     apiToken: {
       salt: env('API_TOKEN_SALT'),
